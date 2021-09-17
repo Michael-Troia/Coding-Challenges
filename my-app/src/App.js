573b9a7521b4b6eb56b9a288accd7ba536937dfc
Michael Troia <troia.michael.a@gmail.com>
2021-09-17 12:37:22 -0400

    rRefactored onClick to use named function

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,11 +2,16 @@ import React from 'react';
 
 const App = () => {
 
-  const exampleText = 'Click Here!'
+  const exampleText = 'Click Here!';
+
+  const handleClick = ()=> {
+    alert('A named function was used to show this alert');
+  }
+
   return (
     <div>
       <h1>Hello World</h1>
-      <h2 onClick={()=> alert('You have clicked!')}>
+      <h2 onClick={handleClick}>
         {exampleText}
       </h2>
     </div>
@@ -14,3 +19,5 @@ const App = () => {
 };
 
 export default App;
+
+// React includes event handlers like onClick, onPress, and onSubmit
